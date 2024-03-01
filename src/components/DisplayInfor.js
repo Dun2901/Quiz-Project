90145f50a473447aace560d410ff46c4fd8991c4
KoPhaiDun <lamchidung373@gmail.com>
2024-03-02 01:28:23 +0700

    add sass

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./DisplayInfor.scss";
 
 class DisplayInfor extends React.Component {
   state = {
@@ -17,7 +18,7 @@ class DisplayInfor extends React.Component {
     // console.log(listUsers);
 
     return (
-      <div>
+      <div className="display-infor-container">
         <div>
           <button
             onClick={() => {
