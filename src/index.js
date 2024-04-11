ebb77a430b22bb77a6a2c33bf1a6d51e23894c2a
KoPhaiDun <lamchidung373@gmail.com>
2024-04-11 12:56:18 +0700

    redux persist

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./Layout";
@@ -11,11 +12,13 @@ import "nprogress/nprogress.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    {/* <React.StrictMode> */}
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
-    {/* </React.StrictMode> */}
+    <PersistGate loading={null} persistor={persistor}>
+      {/* <React.StrictMode> */}
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+      {/* </React.StrictMode> */}
+    </PersistGate>
   </Provider>
 );
 
